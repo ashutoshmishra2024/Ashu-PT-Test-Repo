4f63a13c1ec0b71e12adcbd00b112e1b4f0f6853
ashutoshmishra2024 <ashutosh.mishra@cognizant.com>
2024-10-25 02:37:30 +0530

    Update python-script-test-2.py

--- python-script-test-2.py
+++ python-script-test-2.py
@@ -3,5 +3,5 @@ if 5 > 2:
   print("Five is greater than two!")
 
 # Output of ls command
-#>>> import subprocess
-# subprocess.run(['ls', '-al'])
+>>> import subprocess
+subprocess.run(['ls', '-al'])
