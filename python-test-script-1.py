db76ba3af5106ae63473050979a9a5c07cdfc446
ashutoshmishra2024 <ashutosh.mishra@cognizant.com>
2024-10-24 22:30:42 +0530

    Update python-test-script-1.py

--- python-test-script-1.py
+++ python-test-script-1.py
@@ -1,10 +1,2 @@
 # Print hello 
 print("Hello, World!")
-
-# Print numbers
-if 5 > 2:
-  print("Five is greater than two!")
-
-# Output of ls command
-#>>> import subprocess
-# subprocess.run(['ls', '-al'])
