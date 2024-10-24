ee66b1b84405568fb8d8cb2d4f62c4a66fc7e507
ashutoshmishra2024 <ashutosh.mishra@cognizant.com>
2024-10-25 02:56:47 +0530

    Update python-test-script-1.py

--- python-test-script-1.py
+++ python-test-script-1.py
@@ -1,7 +1,6 @@
 # Print hello 
 print("Hello, World!")
 
-# check Current orking directory
 import os
-# get the current working directory
-current_working_directory = os.getcwd()
+cwd = os.getcwd()
+print(cwd)
