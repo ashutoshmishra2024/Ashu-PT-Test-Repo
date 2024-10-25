893a3297529d1117dacfa5749d6ebe67101d1ec9
ashutoshmishra2024 <ashutosh.mishra@cognizant.com>
2024-10-25 17:17:32 +0530

    Update python-script.py

--- python-script.py
+++ python-script.py
@@ -21,3 +21,15 @@ replace_files = {
 
 zip_file_path = 'home/docker/actions-runner/_work/Ashu-PT-Test-Repo/Ashu-PT-Test-Repo/HTMLReport'
 extraction_dir = 'home/docker/actions-runner/_work/Ashu-PT-Test-Repo/Ashu-PT-Test-Repo/HTMLReport'
+
+zip_path = '/home/docker/actions-runner/_work/Ashu-PT-Test-Repo/Ashu-PT-Test-Repo/HTMLReport'
+extract_to = '/home/docker/actions-runner/_work/Ashu-PT-Test-Repo/Ashu-PT-Test-Repo/HTMLReport'
+
+
+# Function to unzip the folder
+def unzip_folder(zip_path, extract_to):
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+zip_ref.extractall(extract_to)
+
+# Use the function
+unzip_folder(zip_file_path, extraction _dir)
